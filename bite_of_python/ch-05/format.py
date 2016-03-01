4fe983e96d04a6908428a54fdd468715e3a192fe
Manoj Shevate <manoj.shevate@gmail.com>
2016-03-01 21:57:54 +0530

    Added formats

---
+++ bite_of_python/ch-05/format.py
@@ -0,0 +1,16 @@
+age = 27
+name = "Manoj"
+
+print "name: {0}, Age: {1}".format(name,age)
+
+# Numbers are optional
+print "name: {}, Age: {}".format(name,age)
+
+# Keyword based formating
+print "name: {name}, Age: {age}".format(age = age,name =name)
+
+# decimal (.) precision of 3 for float '0.333'
+print '{0:.3f}'.format(1.0/3)
+# fill with underscores (_) with the text centered
+# (^) to 11 width '___hello___'
+print '{0:_^11}'.format('hello')

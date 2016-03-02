baf4c4f1b5b3f303eef377467ff00577e37ab331
Manoj Shevate <manoj.shevate@gmail.com>
2016-03-02 23:26:37 +0530

    Integrated HTML along with the form

--- CherryPy/HelloWorld.py
+++ CherryPy/HelloWorld.py
@@ -8,7 +8,15 @@ class HelloWorld(object):
 
 	@cherrypy.expose
 	def index(self):
-		return "Hello World!!"
+		return """<html>
+          <head></head>
+          <body>
+            <form method="get" action="generate">
+              <input type="text" value="8" name="length" />
+              <button type="submit">Give it now!</button>
+            </form>
+          </body>
+        </html>"""
 
 
 	@cherrypy.expose

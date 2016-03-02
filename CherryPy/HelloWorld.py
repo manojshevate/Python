6fc4c87fdd3902956633e0ca7e9c9100b176e55c
Manoj Shevate <manoj.shevate@gmail.com>
2016-03-02 23:21:52 +0530

    Added generate method

--- CherryPy/HelloWorld.py
+++ CherryPy/HelloWorld.py
@@ -1,4 +1,6 @@
 import cherrypy
+import random
+import string
 
 class HelloWorld(object):
 	
@@ -7,6 +9,12 @@ class HelloWorld(object):
 	@cherrypy.expose
 	def index(self):
 		return "Hello World!!"
+
+
+	@cherrypy.expose
+	def generate(self):
+		return ''.join(random.sample(string.hexdigits, 8))
+
 
 if __name__ == '__main__':
 	cherrypy.quickstart(HelloWorld());

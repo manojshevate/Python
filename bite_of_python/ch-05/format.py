f93666a3b8776de7939cc039810217040df31a0a
Manoj Shevate <manoj.shevate@gmail.com>
2016-03-02 17:37:29 +0530

    tripple quotes

--- bite_of_python/ch-05/format.py
+++ bite_of_python/ch-05/format.py
@@ -14,3 +14,12 @@ print '{0:.3f}'.format(1.0/3)
 # fill with underscores (_) with the text centered
 # (^) to 11 width '___hello___'
 print '{0:_^11}'.format('hello')
+
+
+'''This is a multi-line string. This is the first line.
+Basics
+39
+This is the second line.
+"What's your name?," I asked.
+He said "Bond, James Bond."
+'''
